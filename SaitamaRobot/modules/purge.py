5fa0407c946a11505b8091d23849b9d91bf4656c
* <amadeusrobot.notryuk@gmail.com>
2020-11-07 12:04:36 +0530

    Updated purge.py

--- SaitamaRobot/modules/purge.py
+++ SaitamaRobot/modules/purge.py
@@ -6,10 +6,10 @@ from telegram.error import BadRequest
 from telegram.ext import Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from tg_bot import dispatcher, LOGGER
-from tg_bot.modules.disable import DisableAbleCommandHandler
-from tg_bot.modules.helper_funcs.chat_status import user_admin, can_delete
-from tg_bot.modules.log_channel import loggable
+from SaitamaRobot import dispatcher, LOGGER
+from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from SaitamaRobot.modules.helper_funcs.chat_status import user_admin, can_delete
+from SaitamaRobot.modules.log_channel import loggable
 
 
 @run_async
