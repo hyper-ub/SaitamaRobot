7ca82f8d120bf5ff73080646741db722e839fa6e
kushal-modi <70826024+kushal-modi@users.noreply.github.com>
2020-11-07 20:16:46 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -76,7 +76,7 @@ And the following:
     dispatcher.bot.first_name, ""
     if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-SAITAMA_IMG = "https://telegra.ph/0c1732e996a5d3b0ffc55.jpg"
+SAITAMA_IMG = "https://telegra.ph/file/0c1732e996a5d3b0ffc55.jpg"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
 Kora is hosted on one of Kaizoku's Servers and doesn't require any donations as of now but \
