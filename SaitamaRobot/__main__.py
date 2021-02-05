9ddeb413c82dd980b46e5dd58fab0d33df243e7d
kushal-modi <70826024+kushal-modi@users.noreply.github.com>
2021-02-06 00:24:23 +0530

    approval.py added

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -74,7 +74,7 @@ And the following:
     dispatcher.bot.first_name, ""
     if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-PRINCY_IMG = "https://telegra.ph/file/4610db73cff888458f49c.jpg"
+PRINCY_IMG = "https://telegra.ph/file/c2051f68b3ea7ddbc9c8b.jpg"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
 Princy is hosted on one of Kaizoku's Servers and doesn't require any donations as of now but \
